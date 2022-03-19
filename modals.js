37ac7c6941e3b08b534ded5da027878514784f36
Mattheus Faria <mattheusfaria27@gmail.com>
2022-03-18 21:48:53 -0400

    Upload Initial Resume Website Files
    More to come!

---
+++ modals.js
@@ -0,0 +1,27 @@
+//Modal Code from W3SCHOOLS and translated into d3 
+//https://www.w3schools.com/howto/howto_css_modals.asp
+
+d3.selectAll(".buttons")
+    .on("click",function(){
+        console.log(this.id);
+        d3.select("div#" + this.id)
+            .style("display", "block");
+    });
+d3.selectAll(".close")
+    .on("click", function(){
+        d3.selectAll(".modal")
+            .style("display", "none");
+    });
+
+
+
+/*
+
+var modal = document.getElementById("spotVisModal");
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+ }
+}
+
+*/
