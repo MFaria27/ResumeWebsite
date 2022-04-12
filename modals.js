85bc85ee7c1a1e5d4f6129f324153c14237528ef
Mattheus Faria <mattheusfaria27@gmail.com>
2022-04-12 17:10:42 -0400

    Adding Project Modals and minor changes

--- modals.js
+++ modals.js
@@ -1,11 +1,14 @@
 //Modal Code from W3SCHOOLS and translated into d3 
 //https://www.w3schools.com/howto/howto_css_modals.asp
 
+var currentModal = 0;
+
 d3.selectAll(".buttons")
     .on("click",function(){
         console.log(this.id);
         d3.select("div#" + this.id)
             .style("display", "block");
+        currentModal = 2;
     });
 d3.selectAll(".close")
     .on("click", function(){
@@ -13,6 +16,15 @@ d3.selectAll(".close")
             .style("display", "none");
     });
 
+window.onclick = function(event) {
+    if(currentModal == 1){
+        d3.selectAll(".modal")
+            .style("display", "none");
+    }
+    else{
+        currentModal = 1;
+    }
+}
 
 
 /*
